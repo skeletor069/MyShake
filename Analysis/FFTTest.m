6dd750f18ffc001cd43a1ed56d04185c698e6ed8
TJ <skeletor069@gmail.com>
2018-10-05 21:23:42 +0200

    analysis codes updated

--- Analysis/FFTTest.m
+++ Analysis/FFTTest.m
@@ -1,59 +1,59 @@
 % disp('Chamba : ');
-chamba = GetMagnitudes('chamba.txt');
+chamba = GetPeakTabbed('chamba.txt', true);
 % disp('DLU : ');
-dlu = GetMagnitudes('DLU.txt');
+dlu = GetPeakTabbed('DLU.txt', true);
 % disp('IGN : ');
-ign = GetMagnitudes('IGN.txt');
+ign = GetPeakTabbed('IGN.txt', true);
 % disp('VCD : ');
-vcd = GetMagnitudes('VCD.txt');
+vcd = GetPeakTabbed('VCD.txt', true);
 % disp('JHR : ');
-jhr = GetMagnitudes('JHR.txt');
+jhr = GetPeakTabbed('JHR.txt', true);
 % disp('POR : ');
-por = GetMagnitudes('POR.txt');
+por = GetPeakTabbed('POR.txt', true);
 % disp('Chamba 2: ');
-chm2 = GetMagnitudes('CHM2.txt');
+chm2 = GetPeakTabbed('CHM2.txt', true);
 % disp('HAM : ');
-ham = GetMagnitudes('HAM.txt');
+ham = GetPeakTabbed('HAM.txt', true);
 % disp('JMU : ');
-jmu = GetMagnitudes('JMU.txt');
+jmu = GetPeakTabbed('JMU.txt', true);
 
 % disp('#################');
 % disp('Random : ');
-rand = GetMagnitudes('random.txt');
+rand = GetPeakTabbed('random.txt', false);
 % disp('Table : ');
-table = GetMagnitudes('table.txt');
+table = GetPeakTabbed('table.txt', false);
 % disp('Walk : ');
-walk = GetMagnitudes('walk.txt');
+walk = GetPeakTabbed('walk.txt', false);
 
 
 figure; 
-% [r,c] = size(chamba)
+%  [r,c] = size(chamba)
 xAxis = 1:600;
 plot(xAxis, chamba(1:600), 'k');
 hold on;
 % [r,c] = size(dlu)
-% xAxis = 1:r;
+%  xAxis = 1:r;
 plot(xAxis, dlu(1:600), 'r');
-%[r,c] = size(ign)
-% xAxis = 1:r;
+% [r,c] = size(ign)
+%  xAxis = 1:r;
 plot(xAxis, ign(1:600), 'g');
-% [r,c] = size(vcd)
-% xAxis = 1:r;
+%  [r,c] = size(vcd)
+%  xAxis = 1:r;
 plot(xAxis, vcd(1:600), 'b');
-% [r,c] = size(jhr)
-% xAxis = 1:r;
+%  [r,c] = size(jhr)
+%  xAxis = 1:r;
 plot(xAxis, jhr(1:600), 'c');
-% [r,c] = size(por)
-% xAxis = 1:r;
+%  [r,c] = size(por)
+%  xAxis = 1:r;
 plot(xAxis, por(1:600), 'm');
-% [r,c] = size(chm2)
-% xAxis = 1:r;
+%  [r,c] = size(chm2)
+%  xAxis = 1:r;
 plot(xAxis, chm2(1:600), 'y');
-% [r,c] = size(ham)
-% xAxis = 1:r;
+%  [r,c] = size(ham)
+%  xAxis = 1:r;
 plot(xAxis, ham(1:600), 'o');
-% [r,c] = size(jmu)
-% xAxis = 1:r;
+%  [r,c] = size(jmu)
+%  xAxis = 1:r;
 plot(xAxis, jmu(1:600), 'k');
 [r,c] = size(rand)
 xAxis = 1:r;
@@ -66,5 +66,5 @@ xAxis = 1:r;
 plot(xAxis, walk, 'b');
 hold off;
 xlabel('Time') 
-ylabel('Magnitudes') 
+ylabel('FFT') 
 legend('CHAMBA', 'DLU', 'IGN', 'VCD', 'JHR', 'POR', 'CHM2', 'HAM', 'JMU');
--- Analysis/GetPeakTabbed.m
+++ Analysis/GetPeakTabbed.m
@@ -1,14 +1,13 @@
-function [ peak ] = GetPeakTabbed( filename )
-%     fid = fopen(filename,'rt');
-%     tmp = textscan(fid, '%s\t%f\t%f\t%f', 'Headerlines', 10);
-%     % tmp = textscan(fid, '%s %f %f %f');
-%     fclose(fid);
+function [ quake_fft ] = GetPeakTabbed( filename , trim)
     [x, y, z] = LoadFile(filename);
     quake_magnitude = sqrt(x.^2 + y.^2 + z.^2);
+    if(trim)
+        quake_magnitude = quake_magnitude(1:1200);
+    end
     oddRows = quake_magnitude(1:2:end,:);
+    quake_fft = abs(fft(oddRows));
+    disp(filename);
+    disp(max(quake_fft));
     
-    quake_fft = abs(fft(oddRows(500:564)));
-    peak = max(quake_fft);
-    disp(peak);
 end
 
